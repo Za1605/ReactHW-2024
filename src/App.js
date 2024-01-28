89050d1bb054fff74061ac661c68aa3c05b4642b
YuliiaT <115080417+Za1605@users.noreply.github.com>
2024-01-28 23:02:04 +0200

    HW 2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Launches from "./Components/Launches";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+<Launches/>
+      </div>
   );
-}
+};
 
 export default App;
+
+
+
+
+//є API от SpaceX
+//https://api.spacexdata.com/v3/launches/
+//    потрібно вивести всі запуски кораблів окрім 2020 року
+//репрезентувати тільки окремі поля (зазначені в скрнішоті в папці)
---
+++ src/Contstsnts/url.js
@@ -0,0 +1,13 @@
+const baseURL = 'https://api.spacexdata.com/v3'
+
+const launches = '/launches'
+
+const urls ={
+    launches
+}
+
+export {
+    baseURL,
+    urls,
+    launches
+}
